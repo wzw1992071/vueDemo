646a2805578188a0ec33fa4c44bf1f5c443d25f9
wangziwei@zdongpin.com <wangziwei@zdongpin.com>
2018-07-20 09:05:42 +0800

    1

---
+++ src/views/components/AddGood.vue
@@ -0,0 +1,145 @@
+// 添加商品模块
+<template>
+    <div>
+        <el-form :model="item" :rules="rules" label-width="120px" :ref="goodsForm">
+            <div class="GoodsTitle">
+                商品{{index+1}}
+                <span class="deleteGoodsBtn el-icon-circle-close" @click="deleteOneGood(index)"></span>
+            </div>
+            <el-row>
+                <el-col :span="8">
+                <el-form-item label="商品信息：" prop="goods_name">
+                    <el-input v-model="item.goods_name" @change="changeGoodData"></el-input>
+                </el-form-item>
+                </el-col>
+                <el-col :span="8">
+                <el-form-item label="商品分类：" prop="goods_type">
+                    <el-input v-model="item.goods_type" @change="changeGoodData"></el-input>
+                </el-form-item>  
+                </el-col>
+                <el-col :span="7">
+                <el-form-item label="购买数量：" prop="buy_num">
+                    <el-input v-model.number="item.buy_num" @change="changeGoodData"></el-input>
+                </el-form-item>  
+                </el-col>
+            </el-row> 
+                <el-row>
+                <el-col :span="8">
+                <el-form-item label="销售价格：" prop="goods_sell_price">
+                    <el-input v-model="item.goods_sell_price" @change="changeGoodData"></el-input>
+                </el-form-item>
+                </el-col>
+                <el-col :span="8">
+                <el-form-item label="出售单位：" prop="sell_unit">
+                    <el-input v-model="item.sell_unit" @change="changeGoodData"></el-input>
+                </el-form-item>  
+                </el-col>
+                <el-col :span="7">
+                <el-form-item label="商品原价：" prop="original_price">
+                    <el-input v-model="item.original_price" @change="changeGoodData"></el-input>
+                </el-form-item>  
+                </el-col>
+            </el-row> 
+            <el-row>
+                <el-col :span="8">
+                <el-form-item label="供应商铺名：" prop="seller_shop">
+                    <el-input v-model="item.seller_shop" @change="changeGoodData"></el-input>
+                </el-form-item>
+                </el-col>
+                <el-col :span="8">
+                <el-form-item label="供应商地址：" prop="seller_address">
+                    <el-input v-model="item.seller_address" @change="changeGoodData"></el-input>
+                </el-form-item>  
+                </el-col>
+                <el-col :span="7">
+                <el-form-item label="供应商电话：" prop="seller_tel">
+                    <el-input  v-model="item.seller_tel" @change="changeGoodData"></el-input>
+                </el-form-item>  
+                </el-col>
+            </el-row> 
+            
+        </el-form>
+    </div>
+    
+</template>
+<script>
+    export default {
+        name: 'AddGoods',
+        props: ['formData',"index"],
+        data(){
+            return{
+                 // 表单验证规则
+                // rules:{
+                //     goods_name: [
+                //         { required: true, message: '商品名不能为空!', trigger: 'blur' },
+                //     ],
+                //     goods_type: [
+                //         { required: true, message: '商品分类不能为空!', trigger: 'blur' },
+                //     ],
+                //     buy_num: [
+                //         { required: true, message: '够买数量不能为空!', trigger: 'blur' },
+                //         { type: 'number', message: '请输入数字', trigger: 'blur' },
+                //     ],
+                //     goods_sell_price: [
+                //         { required: true, message: '销售价格不能为空!', trigger: 'blur' },
+                //     ],
+                //     sell_unit: [
+                //         { required: true, message: '出售单位不能为空!', trigger: 'blur' },
+                //     ],
+                //     original_price: [
+                //         { required: true, message: '商品原价不能为空!', trigger: 'blur' },
+                //     ],
+                //     seller_shop: [
+                //         { required: true, message: '供应商铺名不能为空!', trigger: 'blur' },
+                //     ],
+                //     seller_address: [
+                //         { required: true, message: '供应商地址不能为空!', trigger: 'blur' },
+                //     ],
+                //     seller_tel: [
+                //         { required: true, message: '供应商电话不能为空!', trigger: 'blur' },
+                //         // { type:'mobile', message: '请输入正确的电话！', trigger: 'blur' },
+                //         { len:11, message: '请输入正确的电话！', trigger: 'blur' }
+                //     ],
+                // }
+            }
+        },
+        computed:{
+            item:function(){
+                return JSON.parse(JSON.stringify(this.formData))
+            },
+            goodsForm:function(){
+                return `goodsForm${this.index}`
+            }
+        },
+        methods: {
+            deleteOneGood(){
+                this.$emit('deleteOneGood', this.index)
+            },
+            changeGoodData(){
+                this.$emit('changeGoodData',this.item,this.index)
+            },
+            validateForm(){
+                let flag
+                this.$refs[this.goodsForm].validate((valid) => {
+                    flag = valid
+                });
+                return flag
+            }
+        }
+    }
+</script>
+<style scoped lang="less">
+
+    .GoodsTitle{
+        font-size: 16px;
+        margin: 10px 0;
+        .deleteGoodsBtn{
+        cursor: pointer;
+            &:hover{
+                color: #E81123;
+            }
+        }
+    }
+</style>
+
+
---
+++ src/views/components/moreChoice.vue
@@ -0,0 +1,105 @@
+
+import { create } from 'domain';
+<template>
+  <div class="mainBox">
+    <el-input  v-model="inputValue" @change="selectData" @focus="toggerTig(true)" ></el-input>
+    <div class="PromptBox" v-if="PromptShow">
+      <div class="PromptHead">
+        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+        <div class="sureBtn">
+          <el-button type="text" @click="toggerTig(false)">确定</el-button>
+        </div>
+        
+      </div>
+
+      <el-checkbox-group v-model="choiceData" @change="handleCheckedCitiesChange">
+        <el-checkbox v-for="item in selectInfo" :label="item.id" :key="item.id">{{item.name}}</el-checkbox>
+      </el-checkbox-group>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MoreChoice',
+  props: ['info'],
+  data () {
+    return {
+      checkAll: false,
+      PromptShow:false,
+      inputValue:"",
+      choiceData:[],//多选选中数据
+      isIndeterminate: false,//是否全选
+      selectAllInfo:[
+        {name:"小明",id:1},{name:"张红",id:2},{name:"小兰",id:3}
+      ],
+      AllCheckedData:[1,2,3],//全选的数据
+      selectInfo:[]
+    }
+  },
+  watch:{
+    inputValue(val, oldVal){
+      this.selectData()
+    }
+  },
+  methods:{
+    toggerTig(flage){
+      this.PromptShow=flage
+    },
+    handleCheckAllChange(val){
+      this.choiceData = val ? this.AllCheckedData : [];
+      this.isIndeterminate = false;
+    },
+    handleCheckedCitiesChange(value){
+      let checkedCount = value.length;
+      this.checkAll = checkedCount === this.selectAllInfo.length;
+      this.isIndeterminate = checkedCount > 0 && checkedCount < this.selectAllInfo.length;
+    },
+    selectData(){
+      var newArr=[]
+      this.selectAllInfo.forEach(item => {
+        if(item.name.indexOf(this.inputValue)>-1){
+          newArr.push(item)
+        }
+      });
+      this.selectInfo = newArr;
+    }
+
+  },
+  created(){
+    // console.log(this.info)
+    // this.selectData()
+    this.selectInfo = this.selectAllInfo;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .mainBox{
+    width:100%;
+    .PromptBox{
+      width: 100%;
+      background: #aaa
+    }
+    .PromptHead{
+      margin-top: 3px;
+      margin-left: 5px;
+      padding: 8px 0;
+      position: relative;
+      .sureBtn{
+        position: absolute;
+        right: 10px ;
+        top:0px;
+      }
+    }
+    .el-checkbox-group{
+      .el-checkbox{
+        display: block;
+        margin-left: 5px
+      }
+      
+    }
+  }
+</style>
